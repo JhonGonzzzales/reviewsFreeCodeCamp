e4570ebef0d1127a27aaef5debb7d240bedf94be
Jhon Gonzales <jhonherbertgonzaleshurtado@gmail.com>
2026-07-15 19:10:30 -0400

    Construyendo un generador de oraciones!

---
+++ javascript/sentence-maker.js
@@ -0,0 +1,21 @@
+let adjective = "giant";
+let noun = "cat";
+let verb = "dodge";
+let place = "the Moon";
+let adjective2 = "cosmic";
+let noun2 = "asteroid";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "spicy";
+noun = "taco";
+verb = "explode";
+place = "grandma's kitchen";
+adjective2 = "flying";
+noun2 = "frying pan";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
