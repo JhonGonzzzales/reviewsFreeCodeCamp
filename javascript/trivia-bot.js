78d3e87ae7e7f94eaade60aa4b7b6346cbdaab96
Jhon Gonzales <jhonherbertgonzaleshurtado@gmail.com>
2026-07-15 18:58:09 -0400

    Construyendo un bot de trivia!

---
+++ javascript/trivia-bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Sir bot";
+const botLocation = "the world";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Fun Fact: " + favoriteLanguage + " was created in just 10 days in May 1995 by Brendan Eich. He designed the entire language in less than two weeks while working at Netscape!";
+
+console.log(codingFact);
+
+codingFact = "Fun Fact: In " + favoriteLanguage + ", typeof null returns 'object'. This is actually a 30-year-old bug from the very first version of the language, but it can never be fixed because doing so would break millions of websites!";
+
+console.log(codingFact);
+
+codingFact = "Fun Fact: " + favoriteLanguage + " and Java are completely unrelated! As the famous saying goes: 'Java is to " + favoriteLanguage +" what car is to carpet.' The name was chosen purely as a marketing stunt in 1995 to ride the wave of Java's popularity.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
